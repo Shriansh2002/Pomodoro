01478652517ad160bb2dc3aa316269b671922f62
Shriansh Agarwal <41548480+Shriansh2002@users.noreply.github.com>
2023-03-08 23:08:44 +0530

    Setup react-hot-toast

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,14 @@
-import '@/styles/globals.css'
+// Toaster for react-hot-toast
+import { Toaster } from 'react-hot-toast';
+
+// Global styles
+import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Toaster />
+      <Component {...pageProps} />;
+    </>
+  );
 }
