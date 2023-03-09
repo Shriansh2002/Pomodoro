505a6ab68164fd0896992bd0d0e128736870ce8e
Shriansh Agarwal <41548480+Shriansh2002@users.noreply.github.com>
2023-03-09 08:45:38 +0530

    Dark Theme Implementation

--- src/context/BackgroundContext.js
+++ src/context/BackgroundContext.js
@@ -1,11 +1,21 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
 
 const BackgroundContext = createContext();
 
 export function BackgroundProvider({ children }) {
     const [backgroundColor, setBackgroundColor] = useState('#4a4e69');
+    const [darkMode, setDarkMode] = useState(false);
+
+    useEffect(() => {
+        if (darkMode) {
+            setBackgroundColor('#4a4e69');
+        } else {
+            setBackgroundColor('#FFFFFF');
+        }
+    }, [darkMode]);
+
     return (
-        <BackgroundContext.Provider value={{ backgroundColor, setBackgroundColor }}>
+        <BackgroundContext.Provider value={{ backgroundColor, setBackgroundColor, setDarkMode }}>
             {children}
         </BackgroundContext.Provider>
     );
